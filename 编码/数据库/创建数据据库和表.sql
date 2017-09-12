d46925c186c65109c2d979c628a18684e7562eba
prayjourney <2247359268@qq.com>
2017-09-12 22:00:22 +0800

    添加了创建数据库，插入数据的SQL语句.

---
+++ 编码/数据库/创建数据据库和表.sql
@@ -0,0 +1,44 @@
+-- 导出 aboutstudent 的数据库结构
+CREATE DATABASE IF NOT EXISTS `aboutstudent` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `aboutstudent`;
+
+-- 导出  表 aboutstudent.course 结构
+CREATE TABLE IF NOT EXISTS `course` (
+  `cno` varchar(5) NOT NULL COMMENT '课程号',
+  `cname` varchar(10) NOT NULL COMMENT '课程名',
+  `tno` varchar(10) NOT NULL COMMENT '任课老师'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='课程';
+
+
+-- 导出  表 aboutstudent.score 结构
+CREATE TABLE IF NOT EXISTS `score` (
+  `sno` varchar(3) NOT NULL COMMENT '学号',
+  `cno` varchar(5) NOT NULL COMMENT '课程号',
+  `degree` decimal(10,1) NOT NULL COMMENT '分数'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='分数';
+
+
+-- 导出  表 aboutstudent.student 结构
+CREATE TABLE IF NOT EXISTS `student` (
+  `sno` varchar(3) NOT NULL COMMENT '学号',
+  `sname` varchar(4) NOT NULL COMMENT '学生姓名',
+  `ssex` varchar(2) NOT NULL COMMENT '学生性别',
+  `sbirthday` datetime DEFAULT NULL COMMENT '生日',
+  `class` varchar(5) DEFAULT NULL COMMENT '班级'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='学生信息';
+
+
+-- 导出  表 aboutstudent.teacher 结构
+CREATE TABLE IF NOT EXISTS `teacher` (
+  `tno` varchar(3) NOT NULL COMMENT '老师编号',
+  `tname` varchar(4) NOT NULL COMMENT '老师名称',
+  `tsex` varchar(2) NOT NULL COMMENT '性别',
+  `tbirthday` datetime NOT NULL COMMENT '生日',
+  `prof` varchar(6) DEFAULT NULL COMMENT '级别',
+  `depart` varchar(10) NOT NULL COMMENT '部门'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='教师';
+
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ 编码/数据库/添加数据.sql
@@ -0,0 +1,54 @@
+insert into student (sno,sname,ssex,sbirthday,class) values (108 ,'曾华' ,'男' ,'1977-09-01',95033);
+
+insert into student (sno,sname,ssex,sbirthday,class) values (105 ,'匡明' ,'男' ,'1975-10-02',95031);
+
+insert into student (sno,sname,ssex,sbirthday,class) values (107 ,'王丽' ,'女' ,'1976-01-23',95033); 
+
+insert into student (sno,sname,ssex,sbirthday,class) values (101 ,'李军' ,'男' ,'1976-02-20',95033); 
+
+insert into student (sno,sname,ssex,sbirthday,class) values (109 ,'王芳' ,'女' ,'1975-02-10',95031); 
+
+insert into student (sno,sname,ssex,sbirthday,class) values (103 ,'陆君' ,'男' ,'1974-06-03',95031); 
+
+
+insert into course(cno,cname,tno)values ('3-105' ,'计算机导论',825);
+
+insert into course(cno,cname,tno)values ('3-245' ,'操作系统' ,804);
+
+insert into course(cno,cname,tno)values ('6-166' ,'数据电路' ,856);
+
+insert into course(cno,cname,tno)values ('9-888' ,'高等数学' ,100);
+
+
+insert into score(sno,cno,degree)values (103,'3-245',86);
+
+insert into score(sno,cno,degree)values (105,'3-245',75);
+
+insert into score(sno,cno,degree)values (109,'3-245',68);
+
+insert into score(sno,cno,degree)values (103,'3-105',92);
+
+insert into score(sno,cno,degree)values (105,'3-105',88);
+
+insert into score(sno,cno,degree)values (109,'3-105',76);
+
+insert into score(sno,cno,degree)values (101,'3-105',64);
+
+insert into score(sno,cno,degree)values (107,'3-105',91);
+
+insert into score(sno,cno,degree)values (108,'3-105',78);
+
+insert into score(sno,cno,degree)values (101,'6-166',85);
+
+insert into score(sno,cno,degree)values (107,'6-106',79);
+
+insert into score(sno,cno,degree)values (108,'6-166',81); 
+
+
+insert into teacher(tno,tname,tsex,tbirthday,prof,depart) values (804,'李诚','男','1958-12-02','副教授','计算机系');
+
+insert into teacher(tno,tname,tsex,tbirthday,prof,depart) values (856,'张旭','男','1969-03-12','讲师','电子工程系');
+
+insert into teacher(tno,tname,tsex,tbirthday,prof,depart) values (825,'王萍','女','1972-05-05','助教','计算机系');
+
+insert into teacher(tno,tname,tsex,tbirthday,prof,depart) values (831,'刘冰','女','1977-08-14','助教','电子工程系');
