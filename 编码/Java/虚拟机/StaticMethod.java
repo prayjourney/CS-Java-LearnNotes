c431f04dbee59c7de5463f449f0db3f43a7e8847
prayjourney <2247359268@qq.com>
2018-01-17 21:57:31 +0800

    添加了静态方法初始化的测试

---
+++ 编码/Java/虚拟机/StaticMethod.java
@@ -0,0 +1,17 @@
+/**
+ * 测试静态方法的调用
+ * @author renjiaxin
+ *
+ */
+public class StaticMethod {
+	public static void sayHello()
+	{
+		System.out.println("Hello!");
+	}
+
+	public static void main(String[] args) {
+		StaticMethod.sayHello();
+
+	}
+
+}
